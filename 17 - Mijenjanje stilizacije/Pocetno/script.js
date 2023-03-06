78414c696728c9402db52507b5ea0d70294bb5de
Ante <ante.sesar11@gmail.com>
2023-03-06 19:31:12 +0100

    end

--- 17 - Mijenjanje stilizacije/Pocetno/script.js
+++ 17 - Mijenjanje stilizacije/Pocetno/script.js
@@ -1,15 +1,40 @@
-//1
-var ul = document.querySelector("ul");
-ul.style.listStyleType = "decimal";
+// //1
+// var ul = document.querySelector("ul");
+// ul.style.listStyleType = "decimal";
 
-//2
-var info = document.querySelector(".info");
-info.classList.add("hide");
+// //2
+// var info = document.querySelector(".info");
+// info.classList.add("hide");
 
-//3
-var body = document.querySelector("body");
-var sviStilovi = window.getComputedStyle(body, null);
-var stilMarginLeft = sviStilovi.getPropertyValue("margin-left");
-console.log(stilMarginLeft);
+// //3
+// var body = document.querySelector("body");
+// var sviStilovi = window.getComputedStyle(body, null);
+// var stilMarginLeft = sviStilovi.getPropertyValue("margin-left");
+// console.log(stilMarginLeft);
 
 //4
+var pocetnaMargina = sviStilovi.getPropertyValue("margin");
+console.log(pocetnaMargina);
+var marginaBroj = parseInt(pocetnaMargina);
+console.log(marginaBroj);
+
+var animacija = setInterval(funcFrame, 1000);
+var unazad = false;
+
+function funcFrame(){
+    if (unazad == true){
+        marginaBroj--;
+    } else {
+        marginaBroj++;
+    }
+    console.log(marginaBroj);
+    if (marginaBroj == 30) {
+        unazad = true;
+        console.log("<<<<<<")
+    }
+    if (marginaBroj == 0) {
+        unazad = false;
+        console.log(">>>>>>")
+    }
+    body.style.margin = marginaBroj + "px";
+}
