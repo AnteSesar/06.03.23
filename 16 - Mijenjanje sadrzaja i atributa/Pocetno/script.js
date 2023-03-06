c1bf155273b6bdc9a2ce2f1f379fe093bc476dc9
Ante <ante.sesar11@gmail.com>
2023-03-06 19:19:50 +0100

    mid

--- 16 - Mijenjanje sadrzaja i atributa/Pocetno/script.js
+++ 16 - Mijenjanje sadrzaja i atributa/Pocetno/script.js
@@ -1,1 +1,29 @@
+//1
+var h1 = document.querySelector("h1");
+h1.innerText = "Karlovac";
 
+//2
+var divDesciption = document.querySelector(".description");
+divDesciption.innerHTML = "Grad na <h3>5 rijeka</h3>";
+
+//3
+var info = document.querySelector(".info");
+info.textContent = "Peta rijeka je piva.";
+
+
+//4
+var rijeke = ["Kupa", "Korana", "Dobra", "Mreznica", "Karlovačko pivo"];
+var info = document.getElementById("footer");
+
+for(let i = 0; i < rijeke.length; i++)
+{
+    footer.querySelectorAll("li")[i].innerText = rijeke[i];
+}
+
+//5
+//var divElement = document.getElementById("element");
+var myPageOffsetY = 1300;
+var myScrollTop = 200;
+var myKorisnikEkran = 730;
+
+var visljivY = myPageOffsetY - myScrollTop - myKorisnikEkran
--- 17 - Mijenjanje stilizacije/Pocetno/script.js
+++ 17 - Mijenjanje stilizacije/Pocetno/script.js
@@ -1,1 +1,15 @@
+//1
+var ul = document.querySelector("ul");
+ul.style.listStyleType = "decimal";
 
+//2
+var info = document.querySelector(".info");
+info.classList.add("hide");
+
+//3
+var body = document.querySelector("body");
+var sviStilovi = window.getComputedStyle(body, null);
+var stilMarginLeft = sviStilovi.getPropertyValue("margin-left");
+console.log(stilMarginLeft);
+
+//4
